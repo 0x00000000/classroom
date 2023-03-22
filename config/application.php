6bc533d2904cdc72e0e48a43ed5561da6b6aa482
Sergey Kaspari <s.kaspari@icecat.biz>
2023-03-22 23:49:24 +0200

    Updated config.

--- config/application.php
+++ config/application.php
@@ -5,5 +5,5 @@ return array(
     // Must be set if application is not in site's root. F e '/b2b/public'
     'urlPrefix' => '',
     // Must be set if application is not in site's root.
-    // 'baseUrl' => 'http://example.com/b2b/public',
+    // 'baseUrl' => 'http://example.com/public',
 );
